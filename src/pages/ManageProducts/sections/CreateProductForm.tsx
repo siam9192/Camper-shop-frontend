0eb5378b19448dd6607f50a6a527af34b1369442
siam9192 <ahsiam999@gmail.com>
2024-07-11 03:56:27 +0600

    add product form added

--- src/pages/ManageProducts/sections/CreateProductForm.tsx
+++ src/pages/ManageProducts/sections/CreateProductForm.tsx
@@ -1,62 +1,68 @@
-import React from 'react';
+import { productCategories } from '../../../utils/constant';
+import React, { FormEvent, useState } from 'react';
+// type 
 const CreateProductForm = () => {
+ const [addedImages,addImage] = useState([])
+
+ const handleAddImage = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    const imagesUrl = e.currentTarget.files
+    
+    // addImage([...addImage,...e.currentTarget.files])
+    
+ }
   return (
     <div>
       <div
         id="hs-vertically-centered-modal"
-        className="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
+        className="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none p-10"
       >
-        <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
-          <div className="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
-            <div className="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
-              <h3 className="font-bold text-gray-800 dark:text-white">
-                Modal title
-              </h3>
-              <button
-                type="button"
-                className="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700"
-                data-hs-overlay="#hs-vertically-centered-modal"
-              >
-                <span className="sr-only">Close</span>
-                <svg
-                  className="flex-shrink-0 size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M18 6 6 18"></path>
-                  <path d="m6 6 12 12"></path>
-                </svg>
-              </button>
+         <div className="w-full h-full  bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
+         <h1 className=" text-2xl  md:text-3xl text-black font-extrabold md:p-10 p-5 border-b">Create Product</h1>
+         <form action="" className='md:py-10 md:px-20 p-5 space-y-4'>
+            <div className='grid grid-cols-1  md:grid-cols-2 gap-5'>
+                <div className=' space-y-1'>
+               <label htmlFor="product-name" className='text-black font-semibold'>Product Name:</label>
+               <input type="text" id='product-name' className="py-3 px-4 block w-full border border-gray-300 rounded-lg text-sm text-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Product name"/>
+                </div>
+                <div className=' space-y-1'>
+               <label htmlFor="product-name" className='text-black font-semibold'>Product Category:</label>
+               <select  id='product-name' className="py-3 px-4 block w-full border border-gray-300 rounded-lg text-sm text-black uppercase font-bold focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
+                {
+                    productCategories.map((category,index)=><option value={category} key={index}>{category}</option>)
+                }
+               </select>
+                </div>
+                <div className=' md:col-span-2 space-y-1'>
+                <label htmlFor="description" className='text-black font-semibold'>Product Category:</label>
+                <textarea name="" id="description" className='w-full p-2 rounded-lg min-h-72 border bg-gray-100' placeholder='Write something about product'></textarea>
             </div>
-            <div className="p-4 overflow-y-auto">
-              <p className="text-gray-800 dark:text-neutral-400">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
             </div>
-            <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
-              <button
-                type="button"
-                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-                data-hs-overlay="#hs-vertically-centered-modal"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-              >
-                Save changes
-              </button>
+            <div className='grid grid-cols-2  md:grid-cols-4 gap-5'>
+            <div className=' space-y-1'>
+               <label htmlFor="price" className='text-black font-semibold'>Product Price:</label>
+               <input type="text" id='price' className="py-3 px-4 block w-full border border-gray-300 rounded-lg text-sm text-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Enter product price"/>
+                </div>
+                <div className=' space-y-1'>
+               <label htmlFor="price" className='text-black font-semibold'>Available Stock:</label>
+               <input type="text" id='price' className="py-3 px-4 block w-full border border-gray-300 rounded-lg text-sm text-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Enter available stock"/>
+                </div>
+                <div className=' space-y-1'>
+               <label htmlFor="price" className='text-black font-semibold'>Product Ratting:</label>
+               <input type="text" id='price' className="py-3 px-4 block w-full border border-gray-300 rounded-lg text-sm text-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Enter Product ratting"/>
+                </div>
+                <div className=' space-y-1'>
+               
+                </div>
             </div>
-          </div>
+            <div className=''>
+            <label htmlFor="file-input" className="sr-only">Choose file</label>
+  <input onChange={handleAddImage} type="file" multiple name="file-input" id="file-input" className="block w-1/3 border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400
+    file:bg-gray-50 file:border-0
+    file:me-4
+    file:py-3 file:px-4
+    dark:file:bg-neutral-700 dark:file:text-neutral-400"/>
+            </div>
+         </form>
         </div>
       </div>
     </div>
